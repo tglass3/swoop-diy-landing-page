5b8f5aad51f0051a5fb7cbc747999853846c6fe1
tglass3 <120339411+tglass3@users.noreply.github.com>
2023-05-09 21:09:28 +0000

    More stuff added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 // Check JS is Connected
 console.log('👍 JS Connected');
 
+const beegees = ['bg0', 'bg1', 'bg2'];
+const limit = beegees.length;
+const randNum = Math.floor(Math.random()*limit);
+
+const body = document.querySelector('body');
+
+body.classList.add(beegees[randNum]);
+
 // get all the card
 document.querySelectorAll('.card').forEach( item => {
     
