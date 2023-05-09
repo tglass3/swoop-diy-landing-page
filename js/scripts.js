6a55a7b97d3806e98e88643684754600b20e78b9
tglass3 <120339411+tglass3@users.noreply.github.com>
2023-05-09 21:43:44 +0000

    Pasted stuff

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,21 @@
 console.log('👍 JS Connected');
 
 const beegees = ['bg0', 'bg1', 'bg2'];
-const limit = beegees.length;
-const randNum = Math.floor(Math.random()*limit);
+const limitB = beegees.length;
+const randNumB = Math.floor(Math.random()*limitB);
+const swoops = ['https://assets.codepen.io/16425/swoop.png',
+                'https://assets.codepen.io/16425/swoop+2.jpg',
+                'https://assets.codepen.io/16425/swoop3.jpg'];
+const limitS = swoops.length;
+const randNumS = Math.floor(Math.random()*limitS);
 
 const body = document.querySelector('body');
+const frontImg = document.querySelector('.front img');
 
-body.classList.add(beegees[randNum]);
+body.classList.add(beegees[randNumB]);
+
+frontImg.src = swoops[randNumS];
+
 
 // get all the card
 document.querySelectorAll('.card').forEach( item => {
